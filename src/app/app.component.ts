ff952fc35d475d7c6b76b3ff983e6d2e3139c538
Liz <51245710+eahowell@users.noreply.github.com>
2024-11-10 17:43:21 -0600

    Pass dialog to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+// src/app/app.component.ts
 import { Component } from '@angular/core';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'myFlix-Angular-client';
+  title = 'myFlix';
+
+  constructor(public dialog: MatDialog) {}
+  openUserRegistrationDialog(): void {
+    this.dialog.open(UserRegistrationFormComponent, {
+      width: '280px'
+    });
+  }
 }
