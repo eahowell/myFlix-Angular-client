a2a0ba84d16b43d3dee63575b6f5d6865759f84c
Liz <51245710+eahowell@users.noreply.github.com>
2024-11-27 13:02:50 -0600

    Add tooltips to buttons

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatTooltipModule} from '@angular/material/tooltip';
 import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
 import { LoginFormComponent } from '../login-form/login-form.component';
 
@@ -7,6 +12,8 @@ import { LoginFormComponent } from '../login-form/login-form.component';
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.scss',
+  standalone: true,
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, CommonModule, MatTooltipModule],
 })
 export class WelcomePageComponent implements OnInit {
   constructor(public dialog: MatDialog) {}
