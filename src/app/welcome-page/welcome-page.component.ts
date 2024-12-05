cd8b1bbcee508b4e85c6c3d24e21e7d351aaa722
Liz <51245710+eahowell@users.noreply.github.com>
2024-12-05 16:46:23 -0600

    Update dialog formatting

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -16,24 +16,35 @@ import { LoginFormComponent } from '../login-form/login-form.component';
   imports: [MatToolbarModule, MatButtonModule, MatIconModule, CommonModule, MatTooltipModule],
 })
 export class WelcomePageComponent implements OnInit {
+  dialogRef: MatDialogRef<any> | null = null;
   constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {}
   openUserRegistrationDialog(): void {
-    const dialogRef = this.dialog.open(UserRegistrationFormComponent, {
+    this.dialogRef = this.dialog.open(UserRegistrationFormComponent, {
       maxWidth: '450px',
       width: '95%',
       maxHeight: '100vh',
       autoFocus: false,
       panelClass: 'registration-dialog-container',
+      ariaLabel: 'User Registration Dialog',
+      role: 'dialog',
+    });
+    this.dialogRef.afterClosed().subscribe(() => {
+      this.dialogRef = null;
     });
   }
   openLoginDialog(): void {
-    const dialogRef = this.dialog.open(LoginFormComponent, {
+    this.dialogRef = this.dialog.open(LoginFormComponent, {
       maxWidth: '450px',
       width: '95%',
       maxHeight: '100vh',
       autoFocus: false,
+      ariaLabel: 'Login Dialog',
+      role: 'dialog',
+    });
+    this.dialogRef.afterClosed().subscribe(() => {
+      this.dialogRef = null;
     });
   }
 }
