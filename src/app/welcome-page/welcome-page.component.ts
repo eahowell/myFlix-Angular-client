38a07a196e8cbb260e09c535e9ebeb4d6c569e7c
Liz <51245710+eahowell@users.noreply.github.com>
2024-12-09 16:23:14 -0600

    Update Welcome Page Display
    Updated the welcome page to display different content based on if user is logged in or not

--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -1,25 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatTooltipModule} from '@angular/material/tooltip';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
 import { LoginFormComponent } from '../login-form/login-form.component';
+import { StorageService } from '../local-storage.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
   styleUrl: './welcome-page.component.scss',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, CommonModule, MatTooltipModule],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    CommonModule,
+    MatTooltipModule,
+  ],
 })
-export class WelcomePageComponent implements OnInit {
+export class WelcomePageComponent implements OnInit, OnDestroy {
+  username: string | null = null;
   dialogRef: MatDialogRef<any> | null = null;
-  constructor(public dialog: MatDialog) {}
+  private subscriptions: Subscription[] = [];
 
-  ngOnInit(): void {}
+  constructor(
+    private storageService: StorageService,
+    public dialog: MatDialog,
+    public router: Router
+  ) {}
+
+  ngOnInit(): void {
+    // Subscribe to username changes
+    this.subscriptions.push(
+      this.storageService.watchUsername().subscribe((username) => {
+        this.username = username;
+      })
+    );
+  }
   openUserRegistrationDialog(): void {
     this.dialogRef = this.dialog.open(UserRegistrationFormComponent, {
       maxWidth: '450px',
@@ -46,5 +69,12 @@ export class WelcomePageComponent implements OnInit {
     this.dialogRef.afterClosed().subscribe(() => {
       this.dialogRef = null;
     });
+  }
+  ngOnDestroy() {
+    this.subscriptions.forEach((sub) => sub.unsubscribe());
+  }
+  onLogout(): void {
+    this.storageService.clearLocalStorage();
+    // this.router.navigate(['welcome']);
   }
 }
